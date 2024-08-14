37cd48742ca30739048fd7e48d326e0b9ca3063a
Igor Recio <igor.recio.h@gmail.com>
2024-08-14 21:55:48 +0200

    Update install_packages.sh

--- toolchains/ubuntu/install_packages.sh
+++ toolchains/ubuntu/install_packages.sh
@@ -4,4 +4,4 @@ sudo apt-get --yes install build-essential ninja-build
 sudo apt-get --yes install libglfw3-dev libopengl-dev libfftw3-dev libfftw3-single3 libvolk2-dev libzstd-dev libcpu-features-dev
 
 # install mako for volk build
-python -m pip install mako --quiet
+python3 -m pip install mako --quiet
